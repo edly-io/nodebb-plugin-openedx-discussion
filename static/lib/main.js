a8919d15cecac9017709ef78eb6b85aa14f760ff
danial <danialmalik321@gmail.com>
2019-05-07 12:27:38 +0500

    Add methods docstrings

--- static/lib/main.js
+++ static/lib/main.js
@@ -2,6 +2,10 @@
 
 $(document).ready(function () {
 	$(window).on('action:app.loggedOut', function (event, data) {
+		/**
+		 *	Redirect user to Open Edx logout URL to get the user logged out from Open Edx when user logs out
+		 * 	from NodeBB.
+		 */
 		if (logoutURL) {
 			window.location.href = logoutURL;
 		}
